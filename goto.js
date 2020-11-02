c8b40e27ca54390c7fa08c93c20a9a28d52c1448
karintky <karintan221@gmail.com>
2020-11-02 21:30:43 +0800

    Edit JS

---
+++ goto.js
@@ -0,0 +1,14 @@
+$(".reason").slideUp(0);
+$(".reason-box").fadeOut(0);
+
+$("#illegal-1").click(function() {
+  $(".reason1").slideDown(400);
+  $(".box").fadeIn("fast");
+});
+
+$(".reason-box").click(function() {
+ $(".reason1").slideUp(300);
+$(".reason-box").fadeOut(300);
+}).children().click(function() {
+  return false;
+});
