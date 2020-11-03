380ad5889f2b8c08680b70291f2c451027509fab
karintky <karintan221@gmail.com>
2020-11-03 18:28:42 +0800

    edit js

--- goto.js
+++ goto.js
@@ -1,14 +1,15 @@
-$(".reason").slideUp(0);
+$("#reason1").slideUp(0);
 $(".reason-box").fadeOut(0);
 
 $("#illegal-1").click(function() {
-  $(".reason1").slideDown(400);
+  $("#reason1").slideDown(400);
+  $(".reason-box").fadeIn(400);
   $(".box").fadeIn("fast");
 });
 
-$(".reason-box").click(function() {
- $(".reason1").slideUp(300);
-$(".reason-box").fadeOut(300);
-}).children().click(function() {
-  return false;
-});
+// $(".reason-box").click(function() {
+//  $(".reason1").slideUp(300);
+// $(".reason-box").fadeOut(300);
+// }).children().click(function() {
+//   return false;
+// });
