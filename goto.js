88e2a092c25dd607906351c5ca533a7fad165b3e
karintky <karintan221@gmail.com>
2020-11-17 20:21:14 +0800

    EDIT

--- goto.js
+++ goto.js
@@ -1,14 +1,15 @@
+// 按其中一個盒子下去
 $(".reason-box").fadeOut(0);
 $(".reason").each(function () {
   $(this).fadeOut(0);
 });
-
+// 其他消失白色盒子出來
 $(".reason-box").click(function () {
   $(".reason-box").fadeOut(300);
 }).children().click(function () {
   return false;
 });
-
+// 按白色框框以外就彈出去
 $(".reason-box").each(function () {
   $(this).click(function () {
     var show = $(this).attr("reason");
@@ -17,7 +18,7 @@ $(".reason-box").each(function () {
   });
 })
 
-
+// 回到本來的頁面
 $(".button").each(function () {
   $(this).click(function () {
     var show = $(this).attr("data-show");
